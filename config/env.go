77ae53151f2a54365212c4b257a519095fd44775
Wol <contact@wolmer.me>
2024-08-04 11:23:10 +0530

    fix: skip env loading on deployment

--- config/env.go
+++ config/env.go
@@ -2,11 +2,18 @@ package config
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func LoadEnv() {
+	if _, exists := os.LookupEnv("RENDER"); exists {
+		log.Println("Running in Render.com environment. Environment variables are set directly.")
+		return
+	}
+
+	log.Println("Loading .env file..")
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
