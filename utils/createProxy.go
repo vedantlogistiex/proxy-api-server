1e2ebab6a5c158d7361b7222f9927329fd71bdf4
Vedant Kumar <vedant.kumar@logistiex.com>
2025-07-24 02:30:35 +0530

    Update createProxy.go

--- utils/createProxy.go
+++ utils/createProxy.go
@@ -13,19 +13,14 @@ func CreateReverseProxy(target string, pathPrefix string) *httputil.ReverseProxy
 
 	// Modify Response to fix CORS headers
 	proxy.ModifyResponse = func(resp *http.Response) error {
-		// Remove existing CORS headers
 		resp.Header.Del("Access-Control-Allow-Origin")
 		resp.Header.Del("Access-Control-Allow-Credentials")
 		resp.Header.Del("Access-Control-Allow-Headers")
 		resp.Header.Del("Access-Control-Allow-Methods")
 		resp.Header.Del("Access-Control-Expose-Headers")
-
-		// You can set Access-Control-Allow-Origin here OR let Gin middleware set it
-		// If you want to handle dynamic Origin properly, you can skip setting it here
 		return nil
 	}
 
-	// Director to rewrite the URL path
 	proxy.Director = func(req *http.Request) {
 		req.URL.Scheme = url.Scheme
 		req.URL.Host = url.Host
